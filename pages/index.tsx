2e886de22ceaeef1afaa979790b851525d7c3875
fprh13 <fprh13@gmail.com>
2023-11-09 13:54:09 +0900

    [ADD] 최종본

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,25 +24,21 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <div className={styles.title}>
-          <h3> My Counter DApp</h3>
-        </div>
-
-        <div className={styles.description}>
+      <main>
+        <h1 className={styles.title}>My Counter DApp</h1>
+        <p className={styles.description}>
           Contract address: {contractAddress} <br />
           Your address: {myAddress}
-        </div>
-
-        <h3 className={styles.title}>{counter}</h3>
-
+        </p>
+        <h2 className={styles.title}>{counter}</h2>
         <div className={styles.grid}>
           <div className={styles.card}>
             <Web3Button
               contractAddress={contractAddress}
               action={(contract) => contract.call('decrementCounter')}
             >
-              <h1> - </h1>
+              <h1>-</h1>
+              <p>Decrease</p>
             </Web3Button>
           </div>
           <div className={styles.card}>
@@ -58,7 +54,8 @@ const Home: NextPage = () => {
               contractAddress={contractAddress}
               action={(contract) => contract.call('incrementCounter')}
             >
-              <h1> + </h1>
+              <h1>+</h1>
+              <p>Increase</p>
             </Web3Button>
           </div>
         </div>
